cfdef84ba39ad91d8438b5ecc95faabf28bf5a2b
ElisaJTodd <elisajtodd@hotmail.com>
2020-08-22 19:55:21 -0600

    Updates

---
+++ precache-manifest.44fac72e98acee75e97ecd46079602ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f1b588a8b3e891e6f341f4580ccb640",
+    "url": "/elisajtodd/index.html"
+  },
+  {
+    "revision": "96fd0a06972e22f8d84d",
+    "url": "/elisajtodd/static/css/main.b78d6df9.chunk.css"
+  },
+  {
+    "revision": "a37f2ba75c4609f22cf6",
+    "url": "/elisajtodd/static/js/2.d7150d61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/elisajtodd/static/js/2.d7150d61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96fd0a06972e22f8d84d",
+    "url": "/elisajtodd/static/js/main.6a6faf78.chunk.js"
+  },
+  {
+    "revision": "7d4113f76f7a650f3257",
+    "url": "/elisajtodd/static/js/runtime-main.6a49c6fe.js"
+  }
+]);
